dee5a7b6e163847c1edcf9094a9b11fedc839739
Karl Lemfon <karllemfon@gmail.com>
2020-07-22 08:56:20 +0000

    Added new routing information. Going on to create the controllers.

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
 const ctrlMain = require('../controllers/main');
 
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* GET location pages. */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* GET other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
