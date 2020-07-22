34052b3a61d0fb7e336fb63d259f15aa13fd0cfd
Karl Lemfon <karllemfon@gmail.com>
2020-07-22 07:16:35 +0000

    successfully seperated the routes and the controllers.

---
+++ app_server/controllers/main.js
@@ -0,0 +1,8 @@
+const index = (req, res) => {
+    res.render('index', {title: 'Express'});
+};
+
+// Exposing controllers. Take note that in ES5, this should have been module.exports = {};
+export default {
+    index
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const ctrlMain = require('../controllers/main');
 
-const homepageController = (req, res) => {
-  res.render('index', {title: 'Express'});
-};
 
 /* GET home page. */
-router.get('/', homepageController);
+router.get('/', ctrlMain.index);
 
 module.exports = router;
