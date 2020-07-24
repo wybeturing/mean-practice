221bb5fc8943d8c6a33d090ed28d73ff437aa6ae
Karl Lemfon <karllemfon@gmail.com>
2020-07-24 14:52:51 +0000

    Very hectic chapter mate! Did all I could to connect the mongodb database in production mode on Heroku, I guess I will have to try again.

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,16 +1,50 @@
 const mongoose = require('mongoose');
 
 const dbURI = 'mongodb://localhost/Loc8r';
+
+// Adding a conditional to determine the database that is being used based on production/development environment
+/*
+if(process.env.NODE_ENV == 'production'){
+    dbURI = process.env.MONGODB_URI;
+}
+*/
 mongoose.connect(dbURI, {useNewUrlParser: true});
 
 mongoose.connection.on('connected', () => {
     console.log('Mongoose connected to ${dbURI}');
 });
 
-mongoose.connection.on('errir', err => {
+mongoose.connection.on('error', err => {
     console.log('Mongoose connection error:', err);
 });
 
 mongoose.connection.on('disconnected', () => {
     console.log('Mongoose disconnected');
 });
+
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close( () => {
+        console.log('Mongoose disconnected through ${msg}');
+        callback();
+    });
+};
+
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', () => {
+    gracefulShutdown('Heroku app shutdown', () => {
+        process.exit(0);
+    });
+});
+
+require('./locations');
